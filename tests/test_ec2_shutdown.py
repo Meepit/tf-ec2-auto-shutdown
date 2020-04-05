c68f14dc734afc06b1a21a08057f9c3f5c76321c
James Lizamore <jwlizamore@gmail.com>
2020-04-05 22:32:43 +0100

    Add test step

---
+++ tests/test_ec2_shutdown.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestEC2ShutdownLambda(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
